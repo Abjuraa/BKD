4061000e02c5775fc003c1ce93f7a12938072d7e
Bubbuul <107774277+Bubbuul@users.noreply.github.com>
2023-09-06 15:49:23 -0600

    mejoras

--- js/index.js
+++ js/index.js
@@ -131,3 +131,29 @@ window.onload = function () {
 document.querySelector('.modalclose').addEventListener('click', function() {
     document.querySelector('.site').classList.remove('showmodal')
 });
+
+
+//filtro de busqueda
+
+// Obtén referencias a los elementos del DOM
+const filtroInput = document.getElementById('filtro');
+const lista = document.getElementById('lista');
+const elementos = lista.getElementsByTagName('li');
+
+// Agrega un evento de escucha para el input de filtro
+filtroInput.addEventListener('input', filtrarElementos);
+
+function filtrarElementos() {
+  const filtro = filtroInput.value.toLowerCase();
+
+  for (let i = 0; i < elementos.length; i++) {
+    const elemento = elementos[i];
+    const textoElemento = elemento.textContent.toLowerCase();
+
+    if (textoElemento.includes(filtro)) {
+      elemento.style.display = 'block'; // Muestra el elemento
+    } else {
+      elemento.style.display = 'none'; // Oculta el elemento
+    }
+  }
+}
