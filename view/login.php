98802c9230a53fecd6405aa519741bfaac558479
Bubbuul <107774277+Bubbuul@users.noreply.github.com>
2023-09-07 22:19:55 -0500

    ayudaaaa

--- view/login.php
+++ view/login.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hamburgo D.C</title>
     <link rel="stylesheet" href="../css/index.css">
+    <link rel="stylesheet" href="../css/login.css">
     <link href="https://cdn.jsdelivr.net/npm/remixicon@3.2.0/fonts/remixicon.css" rel="stylesheet">
     <link
     rel="stylesheet"
