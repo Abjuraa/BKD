afaa73693b7ff46c84fd99a93f53d6298b5f75d3
libardo.lozanogambas <libardo.lozano.contractor@bbva.com>
2023-10-11 20:06:47 -0500

    ajuste Cart

--- js/cantidad.js
+++ js/cantidad.js
@@ -66,21 +66,65 @@ addToCartBtn.addEventListener("click", (e) => {
     const localSavage = JSON.parse(localStorage.getItem("carrito")) || [];
     localSavage.forEach((p) => {
       console.log(p)
-      products.innerHTML = `
-        <li class="item" id="${p.id}">
-        <div class="thumbnail object-cover">
-            <a href="#"><img src="${p.image}" alt=""></a>
-        </div>
-        <div class="item-content">
-            <p><a href="#">${p.name}</a></p>
-            <span class="price">
-                <span>$${p.total.toFixed(0)}</span>
-                <span class="fly-item"><span>X${p.count}</span></span>
-            </span>
-        </div>
-        <a href="" class="item-remove"><i class="ri-close-line"></i></a>
-      </li>
-    `;
+      
+      let liProductHTML = document.createElement('li');    
+      liProductHTML.className += "item";
+      liProductHTML.id = p.id;
+      let thumbnailHTML = document.createElement('div');
+      thumbnailHTML.className += "thumbnail object-cover";
+      let aHTML = document.createElement('a');
+      aHTML.href = "#";
+      let imgHTML = document.createElement('img');
+      imgHTML.src = p.image;
+      let itemContentHTML = document.createElement('div');
+      itemContentHTML.className += "item-content";
+      let pHTML = document.createElement('p');
+      let a2HTML = document.createElement('a');
+      a2HTML.href = "#";
+      a2HTML.innerHTML = p.name;
+      let spanHTML = document.createElement('span');
+      spanHTML.className += "price";
+      let span2HTML = document.createElement('span');
+      span2HTML.innerHTML = '$'+ p.total.toFixed(0);
+      let span3HTML = document.createElement('span');
+      span3HTML.className += "fly-item";
+      let span4HTML = document.createElement('span');
+      span4HTML.innerHTML = 'X'+ p.count;
+      let a3HTML = document.createElement('a');
+      a3HTML.href = "";
+      a3HTML.className += "item-remove";
+      let iHTML = document.createElement('i');
+      iHTML.className += "ri-close-line";
+      
+      a3HTML.appendChild(iHTML);
+      span3HTML.appendChild(span4HTML);
+      spanHTML.appendChild(span2HTML);
+      spanHTML.appendChild(span3HTML);
+      pHTML.appendChild(a2HTML);
+      itemContentHTML.appendChild(pHTML);
+      itemContentHTML.appendChild(spanHTML);
+      aHTML.appendChild(imgHTML);
+      thumbnailHTML.appendChild(aHTML);
+      liProductHTML.appendChild(thumbnailHTML);
+      liProductHTML.appendChild(itemContentHTML);
+      liProductHTML.appendChild(a3HTML);
+      products.appendChild(liProductHTML);
+      
+    //   products.innerHTML = `
+    //     <li class="item" id="${p.id}">
+    //     <div class="thumbnail object-cover">
+    //         <a href="#"><img src="${p.image}" alt=""></a>
+    //     </div>
+    //     <div class="item-content">
+    //         <p><a href="#">${p.name}</a></p>
+    //         <span class="price">
+    //             <span>$${p.total.toFixed(0)}</span>
+    //             <span class="fly-item"><span>X${p.count}</span></span>
+    //         </span>
+    //     </div>
+    //     <a href="" class="item-remove"><i class="ri-close-line"></i></a>
+    //   </li>
+    // `;
 });
   const cartTotal = document.querySelector(".cart-total");
   const subTotal =  document.querySelector(".subtotal");
